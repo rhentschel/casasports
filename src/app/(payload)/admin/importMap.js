e77f7395150d9655a72ed090ff4300c546d49d4b
rhentschel <>
2026-04-09 14:17:30 +0200

    fix: populate importMap with Logo and Icon components

--- src/app/(payload)/admin/importMap.js
+++ src/app/(payload)/admin/importMap.js
@@ -1,3 +1,8 @@
 /* THIS FILE WAS GENERATED AUTOMATICALLY BY PAYLOAD. */
-/* DO NOT MODIFY IT BECAUSE IT COULD BE REWRITTEN AT ANY TIME. */
-export const importMap = {}
+import { Logo as Logo_0 } from "@/components/admin/Logo"
+import { Icon as Icon_0 } from "@/components/admin/Icon"
+
+export const importMap = {
+  "/components/admin/Logo#Logo": Logo_0,
+  "/components/admin/Icon#Icon": Icon_0,
+}
