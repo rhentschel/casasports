88e60977dd7f5127abff0210b3a7ec1b0531c87d
rhentschel <rhentschel@users.noreply.github.com>
2026-04-24 08:17:23 +0200

    fix: sport.casasports.de in next images remotePatterns
    Next.js Image Loader blockte das Payload-Media absolute URL (https://sport.casasports.de/api/media/file/*.webp),
    dadurch wurden Autor-Bilder in ArticleAuthor-Box leer angezeigt.

--- next.config.ts
+++ next.config.ts
@@ -14,6 +14,10 @@ const nextConfig: NextConfig = {
       },
       {
         protocol: "https",
+        hostname: "sport.casasports.de",
+      },
+      {
+        protocol: "https",
         hostname: "i.ytimg.com",
       },
     ],
