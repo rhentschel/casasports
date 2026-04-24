9fbfe196dae827e4e8e03e773cbeb41c52508c9a
rhentschel <rhentschel@users.noreply.github.com>
2026-04-24 09:59:26 +0200

    fix: spellcheck=false + akzent-dots entfernt
    Firefox zeigte rote wavy-underlines unter Wörtern wie 'Hähnchenbrust', 'Skyr'.
    - spellCheck={false} am ArticleBody div deaktiviert das
    - ::before Akzent-Dots in linker Spalte entfernt (unnoetig, wirken irritierend)

--- src/components/blog/article-body.tsx
+++ src/components/blog/article-body.tsx
@@ -14,6 +14,7 @@ export function ArticleBody({ content }: ArticleBodyProps) {
     <div ref={ref} className="reveal bp-article" id="article-content">
       <div
         className="blog-prose"
+        spellCheck={false}
         dangerouslySetInnerHTML={{ __html: content }}
       />
     </div>
